8bb6629edd78442a3114adf97b762d4fcd10e1bd
TsHristov <tsvetanhristov@yahoo.com>
2018-03-08 15:11:51 +0200

    Add more queries

--- Week1/queries.sql
+++ Week1/queries.sql
@@ -1,3 +1,25 @@
+/* Longest track in milliseconds in the database */
+SELECT Name as "Track Name",
+       min(Milliseconds) as "Length"
+     FROM tracks;
+
+/* Invoices with more than 10 dollars in total amount */
+SELECT InvoiceId as "Invoice ID",
+       Total as "Total"
+     FROM invoices
+     WHERE Total > 10
+     ORDER BY InvoiceId ASC;
+
+/* Invoices with more than 3 tracks in the details */
+SELECT invoices.InvoiceId as "Invoice ID",
+       count(invoice_items.TrackId) as "Tracks count"
+     FROM invoices
+     JOIN
+     invoice_items ON invoices.InvoiceId = invoice_items.InvoiceId
+     GROUP BY invoices.InvoiceId
+     HAVING count(invoice_items.TrackId) > 3
+     ORDER BY count(invoice_items.TrackId) ASC;
+     
 /* Total number of sales per employee. */
 SELECT	employees.FirstName as "First Name",
 	employees.LastName as "Last Name",
@@ -30,3 +52,14 @@ SELECT DISTINCT artists.Name as "Artist Name",
        GROUP BY artists.Name
        HAVING count(tracks.TrackId) > 3
        ORDER BY count(tracks.TrackId) ASC;
+
+/* Longest track per album */
+SELECT albums.Title as "Album Title",
+       tracks.Name as "Track Name",
+       max(tracks.Milliseconds) as "Track Length"
+  FROM albums
+       JOIN
+       tracks ON tracks.AlbumId = albums.AlbumId
+       GROUP BY albums.Title;
+
+       
